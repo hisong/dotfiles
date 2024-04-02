14fc3eaa09512ff99c54951e8d8f40894c1f6afe
hisong <hisong.srv@gmail.com>
2024-04-02 15:18:19 +0900

    プラグインの読み込み順変更

--- .config/nvim/lua/myrc/plugins.lua
+++ .config/nvim/lua/myrc/plugins.lua
@@ -6,8 +6,17 @@ return require('packer').startup(function(use)
     use 'sheerun/vim-wombat-scheme'
     use 'itchyny/lightline.vim'
     use 'nvim-tree/nvim-web-devicons'
-    use 'vim-skk/skkeleton'
-    use 'vim-denops/denops.vim'
-    use 'delphinus/skkeleton_indicator.nvim'
+    use {
+        'vim-skk/skkeleton',
+        requires = {
+            {'vim-denops/denops.vim'},
+        },
+    }
+    use {
+        'delphinus/skkeleton_indicator.nvim',
+        after = {
+            {'vim-skk/skkeleton'},
+        },
+    }
 end)
 
