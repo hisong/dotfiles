46213217457e3596ae16b9229e4b6e4b020f7d92
hisong <hisong@users.noreply.github.com>
2026-06-07 10:23:24 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,36 @@
-vim.cmd('packadd vim-jetpack')
+local is_linux = vim.loop.os_uname().sysname == 'Linux'
 
-require('jetpack.packer').add({
+if is_linux then
+  -- lazy.nvim bootstrap
+  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      'git', 'clone', '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable',
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  require('lazy').setup({
+    { 'vim-denops/denops.vim' },
+    { 'vim-skk/skkeleton' },
+    { 'delphinus/skkeleton_indicator.nvim' },
+    { 'itchyny/lightline.vim' },
+    { 'Shougo/ddc.vim' },
+    { 'Shougo/pum.vim' },
+    { 'Shougo/ddc-ui-pum' },
+    { 'Shougo/ddc-filter-matcher_head' },
+    { 'Shougo/ddc-filter-sorter_rank' },
+  }, {
+    rocks = { enabled = false },
+  })
+
+else
+  vim.cmd('packadd vim-jetpack')
+
+  require('jetpack.packer').add({
     { 'tani/vim-jetpack' },
     { 'vim-denops/denops.vim' },
     { 'vim-skk/skkeleton' },
@@ -11,7 +41,6 @@ require('jetpack.packer').add({
     { 'Shougo/ddc-ui-pum' },
     { 'Shougo/ddc-filter-matcher_head' },
     { 'Shougo/ddc-filter-sorter_rank' },
-    { 'vim-jp/vimdoc-ja' },
-    { 'NLKNguyen/papercolor-theme' },
-})
+  })
+end
 
