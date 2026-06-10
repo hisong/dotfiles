17a0c5edac52a4fb7c9878793ae4e22ab17a7441
hisong <hisong@users.noreply.github.com>
2026-06-11 08:44:58 +0900

    Update keymaps.lua

--- .config/nvim/lua/global/keymaps.lua
+++ .config/nvim/lua/global/keymaps.lua
@@ -1,4 +1,6 @@
 -- keymaps
+local is_linux = vim.loop.os_uname().sysname == 'Linux'
+
 vim.g.mapleader = 'm'
 vim.keymap.set('i', '<C-a>', '<C-G>U<Home>', {silent = true})
 vim.keymap.set('i', '<C-e>', '<C-G>U<End>', {silent = true})
@@ -11,7 +13,11 @@ vim.keymap.set('n', '<Leader>a', 'ggVGy', {silent = true})
 vim.keymap.set('n', '<Leader>t', ':<C-u>tabnew<CR>', {silent = true})
 vim.keymap.set('n', '<Leader>n', ':<C-u>enew<CR>', {silent = true})
 vim.keymap.set('n', '<Leader>/', '/\v')
-vim.keymap.set('n', '<Leader>uu', ':<C-u>JetpackSync<CR>')
 vim.keymap.set('n', 'Y', 'y$')
 vim.keymap.set('', 'J', 'gJ', {silent = true})
 
+if is_linux then
+  vim.keymap.set('n', '<Leader>uu', ':<C-u>Lazy update<CR>')
+else
+  vim.keymap.set('n', '<Leader>uu', ':<C-u>JetpackSync<CR>')
+end
