866d87a0ce5568cc7e718207153128c96f209151
hisong <hisong@users.noreply.github.com>
2026-08-13 13:23:32 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,46 +1,45 @@
 local is_linux = vim.loop.os_uname().sysname == 'Linux'
 
 if is_linux then
-  -- lazy.nvim bootstrap
-  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      'git', 'clone', '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable',
-      lazypath,
+    -- lazy.nvim bootstrap
+    local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            'git', 'clone', '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable',
+            lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    require('lazy').setup({
+        { 'vim-denops/denops.vim' },
+        { 'vim-skk/skkeleton' },
+        { 'delphinus/skkeleton_indicator.nvim' },
+        { 'itchyny/lightline.vim' },
+        { 'Shougo/ddc.vim' },
+        { 'Shougo/pum.vim' },
+        { 'Shougo/ddc-ui-pum' },
+        { 'Shougo/ddc-filter-matcher_head' },
+        { 'Shougo/ddc-filter-sorter_rank' },
+    }, {
+        rocks = { enabled = false },
     })
-  end
-  vim.opt.rtp:prepend(lazypath)
-
-  require('lazy').setup({
-    { 'vim-denops/denops.vim' },
-    { 'vim-skk/skkeleton' },
-    { 'delphinus/skkeleton_indicator.nvim' },
-    { 'itchyny/lightline.vim' },
-    { 'Shougo/ddc.vim' },
-    { 'Shougo/pum.vim' },
-    { 'Shougo/ddc-ui-pum' },
-    { 'Shougo/ddc-filter-matcher_head' },
-    { 'Shougo/ddc-filter-sorter_rank' },
-  }, {
-    rocks = { enabled = false },
-  })
-
 else
-  vim.cmd('packadd vim-jetpack')
+    vim.cmd('packadd vim-jetpack')
 
-  require('jetpack.packer').add({
-    { 'tani/vim-jetpack' },
-    { 'vim-denops/denops.vim' },
-    { 'vim-skk/skkeleton' },
-    { 'delphinus/skkeleton_indicator.nvim' },
-    { 'itchyny/lightline.vim' },
-    { 'Shougo/ddc.vim' },
-    { 'Shougo/pum.vim' },
-    { 'Shougo/ddc-ui-pum' },
-    { 'Shougo/ddc-filter-matcher_head' },
-    { 'Shougo/ddc-filter-sorter_rank' },
-  })
+    require('jetpack.packer').add({
+        { 'tani/vim-jetpack' },
+        { 'vim-denops/denops.vim' },
+        { 'vim-skk/skkeleton' },
+        { 'delphinus/skkeleton_indicator.nvim' },
+        { 'itchyny/lightline.vim' },
+        { 'Shougo/ddc.vim' },
+        { 'Shougo/pum.vim' },
+        { 'Shougo/ddc-ui-pum' },
+        { 'Shougo/ddc-filter-matcher_head' },
+        { 'Shougo/ddc-filter-sorter_rank' },
+    })
 end
 
