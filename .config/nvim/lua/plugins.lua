34931f008dd3175050d2361adef50e682acbcb2c
hisong <hisong@users.noreply.github.com>
2026-08-20 19:45:39 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,5 @@
+local is_linux = vim.loop.os_uname().sysname == 'Linux'
+
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
@@ -11,16 +13,77 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-    { 'vim-denops/denops.vim' },
-    { 'vim-skk/skkeleton' },
-    { 'delphinus/skkeleton_indicator.nvim' },
-    { 'nvim-orgmode/orgmode' },
-    { 'itchyny/lightline.vim' },
-    { 'Shougo/ddc.vim' },
-    { 'Shougo/pum.vim' },
-    { 'Shougo/ddc-ui-pum' },
-    { 'Shougo/ddc-filter-matcher_head' },
-    { 'Shougo/ddc-filter-sorter_rank' },
-}, {
+    -- Denops base
+    {
+        'vim-denops/denops.vim',
+        lazy = false,
+    },
+
+    -- Japanese input
+    {
+        'vim-skk/skkeleton',
+        dependencies = {
+            'vim-denops/denops.vim',
+        },
+    },
+    {
+        'delphinus/skkeleton_indicator.nvim',
+        dependencies = {
+            'vim-skk/skkeleton',
+        },
+    },
+
+    -- Completion base
+    {
+        'Shougo/ddc.vim',
+        dependencies = {
+            'vim-denops/denops.vim',
+        },
+    },
+    {
+        'Shougo/pum.vim'
+    },
+    {
+        'Shougo/ddc-ui-pum',
+        dependencies = {
+            'Shougo/ddc.vim',
+            'Shougo/pum.vim',
+        },
+    },
+    {
+        'Shougo/ddc-filter-matcher_head',
+        dependencies = {
+            'Shougo/ddc.vim',
+        },
+    },
+    {
+        'Shougo/ddc-filter-sorter_rank',
+        dependencies = {
+            'Shougo/ddc.vim',
+        },
+    },
+
+    -- Statusline
+    {
+        'itchyny/lightline.vim',
+    },
+}
+
+-- Orgmode / Linux only
+if is_linux then
+    table.insert(plugins, {
+        'nvim-orgmode/orgmode',
+        dependencies = {
+            'nvim-orgmode/org-bullets.nvim',
+        },
+        event = 'VeryLazy',
+        ft = {'org'},
+        config = function()
+            require('plugin_settings/orgmode')
+        end,
+    })
+end
+
+require('lazy').setup(plugins, {
     rocks = { enabled = false },
 })
