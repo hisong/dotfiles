190a33e9ad60fbf316650adf9647f9de0cff6c6c
hisong <hisong@users.noreply.github.com>
2026-08-20 19:51:03 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,7 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+local plugins = {
     -- Denops base
     {
         'vim-denops/denops.vim',
