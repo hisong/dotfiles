f0b32e895e129260e445f2d8bc8a5989ea3ec1ca
hisong <hisong@users.noreply.github.com>
2026-08-22 14:22:55 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,76 +12,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    -- Denops base
-    {
-        'vim-denops/denops.vim',
-        lazy = false,
-    },
+local plugins = {}
 
-    -- Japanese input
-    {
-        'vim-skk/skkeleton',
-        dependencies = {
-            'vim-denops/denops.vim',
-        },
-    },
-    {
-        'delphinus/skkeleton_indicator.nvim',
-        dependencies = {
-            'vim-skk/skkeleton',
-        },
-    },
-
-    -- Completion base
-    {
-        'Shougo/ddc.vim',
-        dependencies = {
-            'vim-denops/denops.vim',
-        },
-    },
-    {
-        'Shougo/pum.vim'
-    },
-    {
-        'Shougo/ddc-ui-pum',
-        dependencies = {
-            'Shougo/ddc.vim',
-            'Shougo/pum.vim',
-        },
-    },
-    {
-        'Shougo/ddc-filter-matcher_head',
-        dependencies = {
-            'Shougo/ddc.vim',
-        },
-    },
-    {
-        'Shougo/ddc-filter-sorter_rank',
-        dependencies = {
-            'Shougo/ddc.vim',
-        },
-    },
-
-    -- Statusline
-    {
-        'itchyny/lightline.vim',
-    },
-}
+vim.list_extend(plugins, require('plugins.denops'))
+vim.list_extend(plugins, require('plugins.input'))
+vim.list_extend(plugins, require('plugins.completion'))
+vim.list_extend(plugins, require('plugins.markdown'))
+vim.list_extend(plugins, require('plugins.ui'))
 
 -- Orgmode / Linux only
 if is_linux then
-    table.insert(plugins, {
-        'nvim-orgmode/orgmode',
-        dependencies = {
-            'nvim-orgmode/org-bullets.nvim',
-        },
-        event = 'VeryLazy',
-        ft = {'org'},
-        config = function()
-            require('plugin_settings/orgmode')
-        end,
-    })
+    vim.list_extend(plugins, require('plugins.orgmode'))
 end
 
 require('lazy').setup(plugins, {
