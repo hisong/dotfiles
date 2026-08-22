f09a338b9040aba460e5977eebecd06602a9a589
hisong <hisong@users.noreply.github.com>
2026-08-22 14:22:24 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,21 +3,8 @@ vim.opt.fileformats = 'unix,dos,mac'
 -- plugins
 require('plugins')
 
--- deno
-require('plugin_settings/deno')
-
--- skkeleton
-require('plugin_settings/skkeleton')
-
--- skkeleton_indicator
-require('plugin_settings/skkeleton_indicator')
-
--- lightline
-require('plugin_settings/lightline')
-
 -- global_settings
-require('global/settings')
+require('global.settings')
 
 -- global_keymaps
-require('global/keymaps')
-
+require('global.keymaps')
