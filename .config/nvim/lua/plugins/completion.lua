7e9e1700d9ba105ad5990108f0798e8f2e7b0586
hisong <hisong@users.noreply.github.com>
2026-09-03 09:48:23 +0900

    Update completion.lua

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,5 +1,5 @@
 return {
-    -- Completion base
+    -- Completion base (ddc)
     {
         'Shougo/ddc.vim',
         dependencies = {
@@ -11,7 +11,30 @@ return {
             'Shougo/ddc-filter-sorter_rank',
         },
         config = function()
-            require('plugin_settings.ddc')
+            vim.fn['ddc#custom#patch_global']('sources', {'skkeleton'})
+            vim.fn['ddc#custom#patch_global']('sourceOptions', {
+                ['_'] = {
+                    matchers = {'matcher_head'},
+                    sorters = {'sorter_rank'},
+                },
+                ['skkeleton'] = {
+                    mark = 'skkeleton',
+                    matchers = {},
+                    sorters = {},
+                    converters = {},
+                    isVolatile = true,
+                    minAutoCompleteLength = 1,
+                },
+            })
+            vim.fn['ddc#enable']()
+            vim.fn['ddc#custom#patch_global']('ui', 'pum')
+
+            -- keymaps (pum)
+            local opts = {silent = true}
+            vim.keymap.set({'i', 'c'}, '<C-n>', '<Cmd>call pum#map#insert_relative(+1)<CR>', opts)
+            vim.keymap.set({'i', 'c'}, '<C-p>', '<Cmd>call pum#map#insert_relative(-1)<CR>', opts)
+            vim.keymap.set({'i', 'c'}, '<C-y>', '<Cmd>call pum#map#confirm()<CR>', opts)
+            --vim.keymap.set({'i', 'c'}, '<C-e>', '<Cmd>call pum#map#cancel()<CR>', opts)
         end,
     },
 }
