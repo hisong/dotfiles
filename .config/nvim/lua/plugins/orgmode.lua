09db2343538f4755173f3a62756c81c4de580d96
hisong <hisong@users.noreply.github.com>
2026-09-03 09:50:51 +0900

    Update orgmode.lua

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,13 +1,73 @@
+local is_linux = vim.loop.os_uname().sysname == 'Linux'
+
 return {
+    -- Orgmode 本体 / Linux only
     {
         'nvim-orgmode/orgmode',
-        dependencies = {
-            'nvim-orgmode/org-bullets.nvim',
-        },
+        cond = is_linux,
         event = 'VeryLazy',
-        ft = {'org'},
+        ft = { 'org' },
         config = function()
-            require('plugin_settings/orgmode')
+            require('orgmode').setup({
+                org_agenda_files = {
+                    '~/org/inbox.org',
+                    '~/org/todo.org',
+                    '~/org/meetings.org',
+                },
+                org_default_notes_file = '~/org/inbox.org',
+                -- TODO状態
+                org_todo_keywords = {
+                    'TODO(t)',
+                    'WAITING(w)',
+                    '|',
+                    'DONE(d)',
+                    'CANCELLED(c)',
+                },
+                -- 見た目
+                org_startup_indented = true,
+                org_hide_leading_stars = true,
+                org_hide_emphasis_markers = true,
+                -- 完了ログ
+                org_log_done = 'time',
+                org_log_into_drawer = 'LOGBOOK',
+                -- Agenda/Captureを開く見た目
+                win_split_mode = 'float',
+                win_border = 'rounded',
+                -- Capture
+                org_capture_templates = {
+                    i = {
+                        description = 'Inbox',
+                        template = '* %?\n  %u',
+                        target = '~/org/inbox.org',
+                    },
+                    t = {
+                        description = 'ToDo',
+                        template = '* TODO %?\n  %u',
+                        target = '~/org/todo.org',
+                    },
+                    m = {
+                        description = 'Meeting',
+                        template = {
+                            '* %<%Y-%m-%d> %^{会議名}',
+                            '** メモ',
+                            '- ',
+                        },
+                        target = '~/org/meetings.org',
+                    },
+                },
+            })
+            vim.lsp.enable('org')
+        end,
+    },
+
+    -- org-bullets: orgmode に依存するコンパニオン
+    {
+        'nvim-orgmode/org-bullets.nvim',
+        cond = is_linux,
+        dependencies = { 'nvim-orgmode/orgmode' },
+        ft = { 'org' },
+        config = function()
+            require('org-bullets').setup()
         end,
     },
 }
